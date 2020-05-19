5cdfbec24c957878470054a75b421eb4dd04df36
ranckprogram <1474574022@qq.com>
2020-05-19 16:20:06 +0800

    vue cli 3 初体验

---
+++ single-component/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">
+    hhh
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+    };
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="less" scoped>
+</style>
