5f3641e0d64de6e6970fb63fab4c5ae489a546ec
ranckprogram <1474574022@qq.com>
2020-05-26 15:17:38 +0800

    iview bug

--- iview-bug/index.vue
+++ iview-bug/index.vue
@@ -1,22 +1,46 @@
 <template>
   <div class="">
-    
+    <Input
+      v-model="value"
+      placeholder="Enter something..."
+      style="width: 300px"
+      @on-change="handleChange"
+    />
+
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import 'view-design/dist/styles/iview.css';
+
+import { Input } from 'view-design';
+Vue.component('Input', Input);
 export default {
   components: {},
   props: {},
   data() {
     return {
+      value: ""
     };
   },
   computed: {},
-  watch: {},
-  created() {},
-  mounted() {},
-  methods: {}
+  watch: {
+    value(val) {
+      console.log("watch", val)
+    }
+  },
+  created() { },
+  mounted() { },
+  methods: {
+    handleChange(event) {
+      console.log("event start")
+      console.log(event)
+      console.log(event.data)
+      console.log("event end")
+
+    }
+  }
 };
 </script>
 
