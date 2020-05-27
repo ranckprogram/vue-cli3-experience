59a83d25f10eabcd4961cc847fe5a92f986b8cb9
ranckprogram <1474574022@qq.com>
2020-05-27 10:58:30 +0800

    amap test

--- 3.0project/src/components/HelloWorld.vue
+++ 3.0project/src/components/HelloWorld.vue
@@ -1,32 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div id="container"></div>
   </div>
 </template>
 
@@ -35,24 +9,113 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+
+      var colors = {};
+      var GDPSpeed = {
+        // '520000':10,//贵州
+        // '540000':10,//西藏
+        // '530000':8.5,//云南 
+        // '500000':8.5,//重庆
+        // '360000':8.5,//江西
+        // '340000':8.0,//安徽
+        '510000': 7.5,//四川
+        // '350000':8.5,//福建
+        // '430000':8.0,//湖南
+        // '420000':7.5, //湖北
+        // '410000':7.5,//河南
+        // '330000':7.0,//浙江
+        // '640000':7.5,//宁夏
+        // '650000':7.0,//新疆
+        // '440000':7.0,//广东
+        // '370000':7.0,//山东
+        // '450000':7.3,//广西
+        // '630000':7.0,//青海
+        // '320000':7.0,//江苏
+        // '140000':6.5,//山西
+        // '460000':7,// 海南
+        // '310000':6.5,//上海
+        // '110000':6.5, // 北京
+        // '130000':6.5, // 河北
+        // '230000':6, // 黑龙江
+        // '220000':6,// 吉林
+        // '210000':6.5, //辽宁
+        // '150000':6.5,//内蒙古
+        // '120000':5,// 天津
+        // '620000':6,// 甘肃
+        // '610000':8.5,// 甘肃
+        // '710000':2.64, //台湾
+        // '810000':3.0, //香港
+        // '820000':4.7 //澳门
+
+      }
+      var getColorByDGP = function (adcode) {
+        if (!colors[adcode]) {
+          var gdp = GDPSpeed[adcode];
+          if (!gdp) {
+            colors[adcode] = 'transparent'
+          } else {
+            var r = 3;
+            var g = 140;
+            var b = 230;
+            var a = gdp / 10;
+            colors[adcode] = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+          }
+        }
+        return colors[adcode]
+      }
+      var disCountry = new AMap.DistrictLayer.Country({
+        zIndex: 13,
+        SOC: 'CHN',
+        depth: 1,
+        styles: {
+          'nation-stroke': '#fff',
+          'coastline-stroke': 'fff',
+          'province-stroke': 'white',
+          'fill': function (props) {
+            return getColorByDGP(props.adcode_pro)
+          }
+        }
+      })
+
+      var map = new AMap.Map("container", {
+        zooms: [4, 10],
+        center: [106.122082, 33.719192],
+        zoom: 4,
+        isHotspot: false,
+        defaultCursor: 'pointer',
+        layers: [
+          disCountry
+        ],
+        viewMode: '2D',
+        resizeEnable: true
+      })
+
+      var styleName = "amap://styles/" + "darkblue";
+      map.setMapStyle(styleName);
+
+      console.log(map)
+
+    }
+
+
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#container {
+  width: 100%;
+  height: 500px;
+  opacity: .6;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.hello {
+  background: url("./bg0.jpg");
 }
 </style>
