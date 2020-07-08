b917cb0511f4aca5ebb9e9ad70ae98f70c184c98
ranckprogram <1474574022@qq.com>
2020-07-08 18:24:13 +0800

    vue-test 父组件和子组件的生命周期顺序

---
+++ vue-test/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <HelloWorld :message="message" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: {}
+    }
+  },
+  created() {
+    console.log("parent create")
+  
+  },
+  mounted() {
+    console.log("parent mounted")
+  this.message = { message: "message" }
+    console.log("init")
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-test/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="">{{message.message}}</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    message: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {
+    console.log("child create")
+  },
+  mounted() {
+    console.log("child mounted")
+  },
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
